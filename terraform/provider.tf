c5d4e07e6582dce6009ed1538b20312d1368c152
Réda Maizate <reda.maizate@affluences.com>
2023-07-06 11:12:16 +0200

    clean: applied linter

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -15,7 +15,7 @@ terraform {
 }
 
 provider "aws" {
-    region     = var.AWS_REGION
-    access_key = var.AWS_ACCESS_KEY
-    secret_key = var.AWS_SECRET_KEY
+  region     = var.AWS_REGION
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
 }
